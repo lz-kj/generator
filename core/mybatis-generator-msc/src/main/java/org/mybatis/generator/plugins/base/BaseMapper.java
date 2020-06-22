ce150be489be77ec2fbb8287ced843e563d3ad2d
983140265@qq.com <983140265@qq.com>
2020-06-22 18:08:22 +0800

    基于mybatis-generator-core生成BaseMapper

---
+++ core/mybatis-generator-msc/src/main/java/org/mybatis/generator/plugins/base/BaseMapper.java
@@ -0,0 +1,32 @@
+package org.mybatis.generator.plugins.base;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface BaseMapper<Model, E, PK> {
+
+    long countByExample(E example);
+
+    int deleteByExample(E example);
+
+    int deleteByPrimaryKey(PK id);
+
+    int insert(Model record);
+
+    int insertSelective(Model record);
+
+    List<Model> selectByExample(E example);
+
+    List<Model> selectByExampleWithBLOBs(E example);
+
+    Model selectByPrimaryKey(PK id);
+
+    int updateByExampleSelective(@Param("record") Model record, @Param("example") E example);
+
+    int updateByExample(@Param("record") Model record, @Param("example") E example);
+
+    int updateByPrimaryKeySelective(Model record);
+
+    int updateByPrimaryKey(Model record);
+
+}
---
+++ core/mybatis-generator-msc/src/test/java/org/mybatis/generator/MysqlGeneratorTest.java
@@ -0,0 +1,99 @@
+
+package org.mybatis.generator;
+
+import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.StaticJavaParser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mybatis.generator.api.GeneratedJavaFile;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.dom.DefaultJavaFormatter;
+import org.mybatis.generator.config.*;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MysqlGeneratorTest {
+
+    @Test
+    public void testCreate()throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String xml = "/scripts/generatorConfig-mysql.xml";
+//        File configFile = new File(xml);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(MysqlGeneratorTest.class.getResourceAsStream(xml));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    @Test
+    public void testGenerateMyBatis3WithInvalidConfig() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        String xml = "/scripts/generatorConfig-mysql.xml";
+        InputStream inputStream = MysqlGeneratorTest.class.getResourceAsStream(xml);
+        Configuration config = cp.parseConfiguration(inputStream);
+
+        DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+
+        InvalidConfigurationException e =
+                assertThrows(InvalidConfigurationException.class, () -> {
+                    MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+                    myBatisGenerator.generate(null, null, null, false);
+                });
+
+        assertEquals(2, e.getErrors().size());
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("generateJavaFiles")
+    public void testJavaParse(GeneratedJavaFile generatedJavaFile) {
+        DefaultJavaFormatter formatter = new DefaultJavaFormatter();
+
+        ByteArrayInputStream is = new ByteArrayInputStream(
+                formatter.getFormattedContent(generatedJavaFile.getCompilationUnit()).getBytes());
+        try {
+            StaticJavaParser.parse(is);
+        } catch (ParseProblemException e) {
+            fail("Generated Java File " + generatedJavaFile.getFileName() + " will not compile");
+        }
+    }
+
+    public static List<GeneratedJavaFile> generateJavaFiles() throws Exception {
+        List<GeneratedJavaFile> generatedFiles = new ArrayList<>();
+        generatedFiles.addAll(generateJavaFilesMysql());
+        return generatedFiles;
+    }
+
+    private static List<GeneratedJavaFile> generateJavaFilesMysql() throws Exception {
+        return generateJavaFiles("/scripts/generatorConfig-mysql.xml");
+    }
+
+    private static List<GeneratedJavaFile> generateJavaFiles(String configFile) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(MysqlGeneratorTest.class.getResourceAsStream(configFile));
+
+        DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+        myBatisGenerator.generate(null, null, null, false);
+        return myBatisGenerator.getGeneratedJavaFiles();
+    }
+
+}
